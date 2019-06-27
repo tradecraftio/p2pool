214f78b487277173417f6523c321a23f04d6eb6c
Mark Friedenbach <mark@friedenbach.org>
2019-06-27 01:54:12 -0700

    Change to a smaller donation script.

--- p2pool/data.py
+++ p2pool/data.py
@@ -64,7 +64,7 @@ def is_segwit_activated(version, net):
     segwit_activation_version = getattr(net, 'SEGWIT_ACTIVATION_VERSION', 0)
     return version >= segwit_activation_version and segwit_activation_version > 0
 
-DONATION_SCRIPT = '4104ffd03de44a6e11b9917f3a29f9443283d9871c9d743ef30d5eddcd37094b64d1b3d8090496b53256786bf5c82932ec23c3b74d9f05a6f95a8b5529352656664bac'.decode('hex')
+DONATION_SCRIPT = '76a91463753c5b7e79f69644bbbdbf72a4234fb996cff588ac'.decode('hex')
 
 class BaseShare(object):
     VERSION = 0
