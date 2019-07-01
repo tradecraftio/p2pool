c681674408582a40b2fa55cbbada7f5ce50bad3b
Mark Friedenbach <mark@friedenbach.org>
2019-06-30 22:24:19 -0700

    Switch blockchain.info -> freicoin.info for freicoin chain.

--- p2pool/bitcoin/networks/freicoin.py
+++ p2pool/bitcoin/networks/freicoin.py
@@ -20,9 +20,9 @@ POW_FUNC = data.hash256
 BLOCK_PERIOD = 600 # s
 SYMBOL = 'FRC'
 CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Freicoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Freicoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.freicoin'), 'freicoin.conf')
-BLOCK_EXPLORER_URL_PREFIX = 'https://blockchain.info/block/'
-ADDRESS_EXPLORER_URL_PREFIX = 'https://blockchain.info/address/'
-TX_EXPLORER_URL_PREFIX = 'https://blockchain.info/tx/'
+BLOCK_EXPLORER_URL_PREFIX = 'https://freicoin.info/block/'
+ADDRESS_EXPLORER_URL_PREFIX = 'https://freicoin.info/address/'
+TX_EXPLORER_URL_PREFIX = 'https://freicoin.info/tx/'
 SANE_TARGET_RANGE = (2**256//2**52//1000000 - 1, 2**256//2**52 - 1)
 DUMB_SCRYPT_DIFF = 1
 DUST_THRESHOLD = 0.001e8
