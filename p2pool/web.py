d72123e9ca380c3137422b20c095c4a6f3279f4c
Mark Friedenbach <mark@friedenbach.org>
2019-08-11 20:06:50 -0700

    Use 'height' field of share data instead of parse_bip0034.

--- p2pool/web.py
+++ p2pool/web.py
@@ -228,7 +228,7 @@ def get_web_root(wb, datadir_path, bitcoind_getinfo_var, stop_event=variable.Eve
     web_root.putChild('recent_blocks', WebInterface(lambda: [dict(
         ts=s.timestamp,
         hash='%064x' % s.header_hash,
-        number=p2pool_data.parse_bip0034(s.share_data['coinbase'])[0],
+        number=s.share_data['height'],
         share='%064x' % s.hash,
     ) for s in node.tracker.get_chain(node.best_share_var.value, min(node.tracker.get_height(node.best_share_var.value), node.net.CHAIN_LENGTH)) if s.pow_hash <= s.header['bits'].target]))
     web_root.putChild('uptime', WebInterface(lambda: time.time() - start_time))
